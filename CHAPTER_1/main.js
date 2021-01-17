550e999436a5105e9fc4cf27b3d737883b2da5ef
ique-coder <tjdwns0309@gmail.com>
2021-01-17 22:14:49 +0900

    commit on 'master'

--- CHAPTER_1/main.js
+++ CHAPTER_1/main.js
@@ -1,5 +1,9 @@
 //console.log('main.js loaded');
 $(document).ready(function() {
     'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+
+    paper.view.draw();
     console.log('main.js loaded');
 })
